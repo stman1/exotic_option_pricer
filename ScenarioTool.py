e6118ed73102bae4a841a53961840ef7dd69efe9
Stefan Mangold <stefanmangold1@gmail.com>
2022-04-21 14:43:35 +0200

    scenario tool development
    - further development of the scenario tool

--- ScenarioTool.py
+++ ScenarioTool.py
@@ -51,19 +51,19 @@ class Scenario:
         
         # market parameters, read in dictionaries
         for key in market_parameter:
-            self.market[key] = [market_parameter[key]]
+            self.market[key] = {'eval': market_parameter[key]}
             
         # contract parameters, read in dictionaries
         for key in contract_parameter:
-            self.contract[key] = contract_parameter[key]
+            self.contract[key] = {'eval': contract_parameter[key]}
             
         # simulation parameter, read in dictionaries
         for key in simulation_parameter:
-            self.simulation[key] = simulation_parameter[key]            
+            self.simulation[key] = {'eval': simulation_parameter[key]}
             
         # market parameters, read in dictionaries
         for key in calculation_parameter:
-            self.calculation[key] = calculation_parameter[key]
+            self.calculation[key] = {'eval': calculation_parameter[key]}
             
         self.scenario = {'market' : self.market, 
                          'contract' : self.contract, 
@@ -79,68 +79,136 @@ class Scenario:
         if scenario_definition[1] not in self.scenario[scenario_definition[0]]:
             print(f'{scenario_definition[1]} is not a valid parameter in scenario type {scenario_definition[0] }')
             return
-        
-        if isinstance(scenario_definition[2], (int, float)) and not isinstance(scenario_definition[2], bool):
-            if isinstance(scenario_definition[3], (int, float)) and not isinstance(scenario_definition[3], bool):
-                if scenario_definition[2] < scenario_definition[3]:
-                    self.scenario[scenario_definition[0]][scenario_definition[1]][1].\
-                        append([param_val for param_val in range(scenario_definition[2], scenario_definiton[3] + scenario_definiton[4] , scenario_definition[4])])
-                    
+        self.scenario[scenario_definition[0]][scenario_definition[1]]['scenario'] =  scenario_definition[2]
             
         
         
+    def run_scenarios(self):
         
+        # check each of market, contract, calculation, and simulation parameters for scenarios
+        
+        # check market scenario
+        # if market parameters are present, run them
+        # self._run_market_scenario()
+        
+        # check contract scenario
+        # if contract parameters are present, run them
+        contract_scenarios = self._find_scenario('contract')
+        
+        for cs in contract_scenarios:
+            self._run_contract_scenario(cs)
+        
+        # check calculation scenario
+        # if calculation parameters are present, run them
+        # self._run_calculation_scenario()
+
+
+        # check simulation scenario
+        # if simulation parameters are present, run them
+        # self._run_simulation_scenario()        
+        pass
         
     
     
     # run a scenario
-    def run_scenario(self):
+    def _run_single_scenario(self):
         self._generate_asset_paths()
        
-        time_line = linspace(0, self.market['time'], self.simulation['time_steps'])
+        time_line = linspace(0, self.market['time']['eval'], self.simulation['time_steps']['eval'])
        
-        if self.contract['payoff_type'] == PayoffType.EUROPEAN:
+        if self.contract['payoff_type']['eval'] == PayoffType.EUROPEAN:
             option = EuropeanOption(time_line, 
                                      self.asset_paths, 
-                                     self.contract['option_type'], 
-                                     self.contract['strike'], 
-                                     self.market['drift'], 
-                                     self.market['time'])
+                                     self.contract['option_type']['eval'], 
+                                     self.contract['strike']['eval'], 
+                                     self.market['drift']['eval'], 
+                                     self.market['time']['eval'])
            
-        elif self.contract['payoff_type'] == PayoffType.ASIAN:
+        elif self.contract['payoff_type']['eval'] == PayoffType.ASIAN:
             option = AsianOption(time_line, 
                                  self.asset_paths, 
-                                 self.contract['option_type'],
-                                 self.contract['strike_type'],
-                                 self.contract['averaging_type'], 
-                                 self.contract['strike'], 
-                                 self.market['drift'], 
-                                 self.market['time'])
+                                 self.contract['option_type']['eval'],
+                                 self.contract['strike_type']['eval'],
+                                 self.contract['averaging_type']['eval'], 
+                                 self.contract['strike']['eval'], 
+                                 self.market['drift']['eval'], 
+                                 self.market['time']['eval'])
            
-        elif self.contract['payoff_type'] == PayoffType.LOOKBACK:
+        elif self.contract['payoff_type']['eval'] == PayoffType.LOOKBACK:
             option = LookbackOption(time_line, 
                                      self.asset_paths, 
-                                     self.contract['option_type'], 
-                                     self.contract['strike'], 
-                                     self.market['drift'], 
-                                     self.market['time'])
+                                     self.contract['option_type']['eval'], 
+                                     self.contract['strike']['eval'], 
+                                     self.market['drift']['eval'], 
+                                     self.market['time']['eval'])
     
-        num_sims = self.simulation['num_sims']
-        payoff_type = self.contract['payoff_type']
+        num_sims = self.simulation['num_sims']['eval']
+        payoff_type = self.contract['payoff_type']['eval']
         print(f'Number of MC simulations : {num_sims}')
         print(f'{payoff_type} by Monte Carlo: {option.call_price:0.4f}, {option.put_price:0.4f}')
        
        
+
+        
+        
+        
+    def _run_market_scenario(self):
+        # market scenarios always need a regeneration of asset paths
+        pass
+    
+    def _run_simulation_scenario(self):
+        #  simulation scenarios also always need a regeneration of asset paths
+        pass
+    
+    
+    def _run_contract_scenario(self, contract_scenario):
+        # a contract does not need newly generated asset paths
+        
+        try:
+            original_parameter_val = self.contract[contract_scenario[0]]['eval']
+            # unwrap scenario
+            contract_parameter_space = self.contract[contract_scenario[0]]['scenario']
+            for param in contract_parameter_space:
+                # change parameter in scenario
+                self.contract[contract_scenario[0]]['eval'] = param
+                self._run_single_scenario()
+                
+            # reset state of the object    
+            self.contract[contract_scenario[0]]['eval'] = original_parameter_val
+        except IndexError:
+            # reset state of the object    
+            self.contract[contract_scenario[0]]['eval'] = original_parameter_val
+            contract_parameter_space = 'null'
+        
+    
+    def _run_calculation_scenario(self):
+        # calculation scenarios represent a comparison of one calculation method with another
+        pass
+    
+    
+    def _find_scenario(self, parameter_type):
+        scenario_parameter = []
+        for first in self.scenario['contract'].keys():
+            if 'scenario' in list(self.scenario['contract'][first].keys()):
+                scenario_parameter.append([first, list(self.scenario['contract'][first]['scenario'])])
+        return scenario_parameter
+
+        
+    
     def _generate_asset_paths(self):
-        self.asset_paths = simulate_path(self.market['start_val'], 
-                                       self.market['drift'],
-                                       self.market['volatility'],
-                                       self.market['time'],
-                                       self.simulation['time_steps'],
-                                       self.simulation['num_sims'],
-                                       self.simulation['antithetic'])
-               
-           
+        self.asset_paths = simulate_path(self.market['start_val']['eval'], 
+                                       self.market['drift']['eval'],
+                                       self.market['volatility']['eval'],
+                                       self.market['time']['eval'],
+                                       self.simulation['time_steps']['eval'],
+                                       self.simulation['num_sims']['eval'],
+                                       self.simulation['antithetic']['eval'])
+    
+    
+    
+
+        
+             
        
         
 
--- main.py
+++ main.py
@@ -30,107 +30,34 @@ pd.set_option('display.max_rows', 300)
 
 # Global initial market, contract and simulation parameters
 
-spot_price = 100; strike_price = 100; risk_free_rate = 0.05; asset_volatility = 0.20; time_horizon = 1; timesteps = 252; antithetic = True
 
-# Test variance reduction technique: moment-matching 
-#start_time = time.time()
-#phi = moment_matching(0., 1., 1000, 5)
-#asset_paths = simulate_path_moment_matched(spot_price, risk_free_rate, asset_volatility, time_horizon, timesteps, 100)
-#time_step_mean = mean(phi, axis = 0)
-#time_step_std = std(phi, axis = 0)
-#print(shape(phi))
-#print("--- %s seconds ---" % (time.time() - start_time))
+# market parameters
+spot_price = 100 
+risk_free_rate = 0.05 
+asset_volatility = 0.20 
+time_horizon = 1 
 
-# Test asset price path convergence
-#start_time = time.time()
-#asset_price_path_convergence = test_martingale_property_asset_price_path(spot_price, risk_free_rate, asset_volatility, timesteps, antithetic)
-#print("--- %s seconds ---" % (time.time() - start_time))
+# contract parameters
+strike_price = 100 
 
-
-# Validate European option payoff against analytical Black Scholes pricer
-
-# =============================================================================
-# num_sims = 1000000
-# 
-# start_time = time.time()
-# asset_paths = simulate_path(spot_price, risk_free_rate, asset_volatility, time_horizon, timesteps, num_sims, True)
-# time_line = linspace(0, time_horizon, timesteps)
-# option_type = OptionType.CALL
-# european = EuropeanOption(time_line, asset_paths, option_type, strike_price, risk_free_rate, time_horizon)
-# print(f'Number of MC simulations : {num_sims}')
-# print(f'European Option by Monte Carlo: {european.call_price:0.4f}, {european.put_price:0.4f}')
-# print("--- %s seconds ---" % (time.time() - start_time))
-# 
-# # Setup BS analytic pricer
-# 
-# analytic_bs = BS(100, 100, 0.05, 1, 0.2)
-# print(f'Analytic BS Call Option price: {analytic_bs.callPrice:0.4f}')
-# print(f'Analytic BS Put Option price: {analytic_bs.putPrice:0.4f}')
-
+#simulation parameters
+num_simulations = 10000
+timesteps = 252 
+antithetic_flag = True
 
 # Run a scenario
 
-market_parameters = {'start_val': 100, 'drift' : 0.05, 'volatility' : 0.2, 'time' : 1}
+market_parameters = {'start_val': spot_price, 'drift' : risk_free_rate, 'volatility' : asset_volatility, 'time' : time_horizon}
 contract_parameters = { 'payoff_type' : PayoffType.ASIAN,  'strike_type' : StrikeType.FIXED, 'averaging_type' : AveragingType.ARITHMETIC, 'option_type' : OptionType.CALL, 'strike' : 100}
 calculation_parameters = { 'analytic' : True, 'monte_carlo' : False}
-simulation_parameters = {'num_sims': 10000, 'time_steps' : 252, 'antithetic' : True}
+simulation_parameters = {'num_sims': num_simulations, 'time_steps' : timesteps, 'antithetic' : antithetic_flag}
 
 my_scenario = Scenario(market_parameters, contract_parameters, calculation_parameters, simulation_parameters)
 
-my_scenario.define_scenario(['contract', 'strike', 0, 500, 25])
+strike_space = [0, 25, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 350, 375, 400, 425, 450, 475, 500]
+my_scenario.define_scenario(['contract', 'strike', strike_space])
+averaging_space = [AveragingType.ARITHMETIC, AveragingType.GEOMETRIC]
+my_scenario.define_scenario(['contract', 'averaging_type', averaging_space])
 
+my_scenario.run_scenarios()
 
-# my_scenario.run_scenario()
-# 
-# =============================================================================
-
-# =============================================================================
-# # Call the simulation function
-# S = simulate_path(100, 0.05, 0.20, 1, 252, 100000)
-# 
-# # Define parameters
-# K = 100.; r = 0.05; T = 1
-# 
-# # European options
-# 
-# # Calculate the discounted value of the expeced payoff
-# C0 = exp(-r*T) * mean(maximum(S[-1] - K, 0))
-# P0 = exp(-r*T) * mean(maximum(K - S[-1], 0))
-# 
-# # Print the values
-# print(f'European Call Option Value: {C0:0.4f}')
-# print(f'European Put Option Value: {P0:0.4f}')
-# 
-# # Asian options
-# 
-# # Average price
-# A = S.mean(axis=0)   
-#     
-# # Calculate the discounted value of the expeced payoff
-# C0 = exp(-r*T) * mean(maximum(A - K, 0))                # mean is for present value which is for discounting
-# P0 = exp(-r*T) * mean(maximum(K - A, 0))
-# 
-# # Print the values
-# print(f'Asian Call Option Value: {C0:0.4f}')
-# print(f'Asian Put Option Value: {P0:0.4f}')
-# 
-# # Black-Scholes price
-# 
-# bsopt = BS(100, 100, 0.05, 1, 0.2)
-# 
-# header = ['Option Price', 'Delta', 'Gamma', 'Theta', 'Vega', 'Rho']
-# table = [[bsopt.callPrice, bsopt.callDelta, bsopt.gamma, bsopt.callTheta, bsopt.vega, bsopt.callRho]]
-# 
-# print(f'Black-Scholes Call Option Value: {bsopt.callPrice:0.4f}')
-# print(f'Black-Scholes Put Option Value: {bsopt.putPrice:0.4f}')
-# 
-# # Define Asian option payoffs
-# 
-# time_line = linspace(0, T, 252)
-# asianOption = AsianOption(time_line, S, OptionType.CALL, StrikeType.FIXED, AveragingType.ARITHMETIC, 100, r, T)
-# #print(asianOption.call_price)
-# print(f'Arithmetic Asian Option by Monte Carlo: {asianOption.call_price:0.4f}, {asianOption.put_price:0.4f}')
-# asianOption.resetAveragingType(AveragingType.GEOMETRIC)
-# print(f'Geometric Asian Option by Monte Carlo: {asianOption.call_price:0.4f}, {asianOption.put_price:0.4f}')
-# 
-# =============================================================================
